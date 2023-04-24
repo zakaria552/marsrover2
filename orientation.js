798f0f9a1c206275bd01f31a7d50858298e48ef3
zakaria552 <zakaria.farah55@gmail.com>
2023-04-24 08:20:01 +0100

    some changes

--- orientation.js
+++ orientation.js
@@ -2,13 +2,19 @@ const cardinalPoints = ["N", "E", "S", "W"]
 class Orientation {
     constructor(dir) {
         this.dir = dir
-        this.turn = cardinalPoints.indexOf(dir)
+        this.nextDirIndex = cardinalPoints.indexOf(dir)
     }
 
     rotate(rotation) {
-        rotation == "R" ? this.turn = (this.turn + 1) % 4: this.turn = (((this.turn - 1) % 4) +4) % 4
+        if(this.nextDirIndex == 0 && rotation == "L") {
+            this.nextDirIndex = ((this.nextDirIndex - 1) % 4) + 4
+        } else {
+            rotation == "R" ? this.nextDirIndex = (this.nextDirIndex + 1) % 4: this.nextDirIndex = (this.nextDirIndex - 1) % 4
+        }
 
-        this.dir = cardinalPoints[this.turn]
+        // rotation == "R" ? this.nextDirIndex = (this.turn + 1) % 4: this.nextDirIndex = (((this.nextDirIndex - 1) % 4) +4) % 4
+
+        this.dir = cardinalPoints[this.nextDirIndex]
     }
 }  
 
