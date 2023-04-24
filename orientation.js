69acbd63e4542264fcd3b2c7d6a4a57d91b6881b
zakaria552 <zakaria.farah55@gmail.com>
2023-04-24 11:54:21 +0100

    refactored

--- orientation.js
+++ orientation.js
@@ -6,14 +6,7 @@ class Orientation {
     }
 
     rotate(rotation) {
-        if(this.nextDirIndex == 0 && rotation == "L") {
-            this.nextDirIndex = ((this.nextDirIndex - 1) % 4) + 4
-        } else {
-            rotation == "R" ? this.nextDirIndex = (this.nextDirIndex + 1) % 4: this.nextDirIndex = (this.nextDirIndex - 1) % 4
-        }
-
-        // rotation == "R" ? this.nextDirIndex = (this.turn + 1) % 4: this.nextDirIndex = (((this.nextDirIndex - 1) % 4) +4) % 4
-
+        rotation == "R" ? this.nextDirIndex = (this.nextDirIndex + 1) % 4: this.nextDirIndex = ((this.nextDirIndex + 3) % 4)
         this.dir = cardinalPoints[this.nextDirIndex]
     }
 }  
