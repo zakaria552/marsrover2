74902c76c0c5212b058e4b32f39e128abea9536b
zakaria552 <zakaria.farah55@gmail.com>
2023-04-24 11:54:49 +0100

    main setup

--- main.js
+++ main.js
@@ -3,13 +3,10 @@ const Surface = require("./surface")
 const plateau = new Surface(5,5)
 // adding rovers to the world
 const rover = plateau.addRover(1, 2, "N", plateau)
-// const rover2 = plateau.addRover(3, 3, "E", plateau)
-console.log(plateau.getGrid())
+const rover2 = plateau.addRover(3, 3, "E", plateau)
+// sending signals to rover
 rover.sendSignal("LMLMLMLMM")
 console.log(rover.pingLocation())
-console.log(plateau.getGrid())
-
-// rover2.sendSignal("MMRMMRMRRM")
-// console.log(rover2.pingLocation())
-// console.log(plateau.getGrid())
-console.log(-1 % 4)
+// sending signals to rover2
+rover2.sendSignal("MMRMMRMRRM")
+console.log(rover2.pingLocation())
