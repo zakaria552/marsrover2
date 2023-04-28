29974b308dd2e1b7c9c7a612f4facd88b6963102
zakaria552 <zakaria.farah55@gmail.com>
2023-04-28 16:17:29 +0100

    getRoverNextPosition now fetches the position increments

--- classes/position.js
+++ classes/position.js
@@ -1,20 +1,21 @@
 class Position {
-    constructor(x,y) {
-        this.x = x
-        this.y = y
-    }
+     constructor(x,y) {
+          this.x = x
+          this.y = y
+     }
 
-   getNextPosition(xyChanges) {
-        const x = xyChanges[0] + this.x
-        const y = xyChanges[1] + this.y
-        return new Position(x,y)
-   }
-   getX() {
-        return this.x
-   }
-   getY() {
-        return this.y
-   }
+     getRoverNextPosition(orient) {
+          const [xChange, yChange] = orient.getPositionIncrements()
+          let x = this.x + xChange
+          let y = this.y + yChange
+          return new Position(x,y)
+     }
+     getX() {
+          return this.x
+     }
+     getY() {
+          return this.y
+     }
 }
 
 module.exports = Position
