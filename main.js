3f831bf30d907e740407efebe721a8cfbe11f0d0
zakaria552 <zakaria.farah55@gmail.com>
2023-04-28 10:45:09 +0100

    removed comments

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const Surface = require("./classes/surface")
-// instance of the world
+
 const plateau = new Surface(5,5)
-// adding rovers to the world
+
 const rover = plateau.addRover(1, 2, "N", plateau)
 const rover2 = plateau.addRover(3, 3, "E", plateau)
-// sending signals to rover
+
 rover.sendSignal("LMLMLMLMM")
 console.log(rover.pingLocation())
-// sending signals to rover2
+
 rover2.sendSignal("MMRMMRMRRM")
 console.log(rover2.pingLocation())
