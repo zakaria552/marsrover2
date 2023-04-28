0784848e8105789acceeab320d265876e3e7bcfb
zakaria552 <zakaria.farah55@gmail.com>
2023-04-28 10:44:33 +0100

    information is no longer information expert of orientation

--- classes/position.js
+++ classes/position.js
@@ -4,12 +4,9 @@ class Position {
         this.y = y
     }
 
-   getNextPosition(orient) {
-        // ["N", "E", "S", "W"] - "N" -> [x,y]=[0,1]
-        const increments = [[0,1], [1,0], [0,-1], [-1,0]]
-        const i = orient.getCardinalPoints().indexOf(orient.getDir())
-        const x = increments[i][0] + this.x
-        const y = increments[i][1] + this.y
+   getNextPosition(xyChanges) {
+        const x = xyChanges[0] + this.x
+        const y = xyChanges[1] + this.y
         return new Position(x,y)
    }
    getX() {
