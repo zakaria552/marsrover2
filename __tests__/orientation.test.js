da2998da9a95333b2eb29b24aca2b4089e2c3a19
zakaria552 <zakaria.farah55@outlook.com>
2023-04-29 17:43:05 +0100

    variable name changes

--- __tests__/orientation.test.js
+++ __tests__/orientation.test.js
@@ -3,35 +3,35 @@ const Orientation = require("../classes/orientation")
 describe("Orientation class", () => {
     test("create an instance of oerientation given direction", () => {
         const orient = new Orientation("W")
-        expect(orient.dir).toBe("W")
+        expect(orient.currentDirection).toBe("W")
     })
 
     test("changes to the correct orientation after rotating 90 degree to the left", () => {
         const orient = new Orientation("W")
         // facing west turn to the left should give us dir of south
-        orient.rotate("L")
-        expect(orient.dir).toBe("S")
+        orient.changeOrientationByTurn("L")
+        expect(orient.currentDirection).toBe("S")
         //facing south turn to the left should give us dir of east
-        console.log(orient.dir)
-        orient.rotate("L")
-        console.log(orient.dir)
-        expect(orient.dir).toBe("E")
+        console.log(orient.currentDirection)
+        orient.changeOrientationByTurn("L")
+        console.log(orient.currentDirection)
+        expect(orient.currentDirection).toBe("E")
         // // facing east turn to the left should give us dir of north
-        orient.rotate("L")
-        expect(orient.dir).toBe("N")
+        orient.changeOrientationByTurn("L")
+        expect(orient.currentDirection).toBe("N")
     })
     test("change to the correct orientation after rotating 90 degree to the right", () => {
         const orient = new Orientation("W")
         // facing west turn to the right should give us dir of north
-        orient.rotate("R")
-        expect(orient.dir).toBe("N")
+        orient.changeOrientationByTurn("R")
+        expect(orient.currentDirection).toBe("N")
         // facing north turn to the right should give us dir of east
-        console.log(orient.dir)
-        orient.rotate("R")
-        expect(orient.dir).toBe("E")
+        console.log(orient.currentDirection)
+        orient.changeOrientationByTurn("R")
+        expect(orient.currentDirection).toBe("E")
         // facing east turn to the right should give us dir of south
-        orient.rotate("R")
-        expect(orient.dir).toBe("S")
+        orient.changeOrientationByTurn("R")
+        expect(orient.currentDirection).toBe("S")
     })
 
 })
